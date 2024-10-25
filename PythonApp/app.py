f895be2554eba9a06f4dea0ab206762f4d594d97
Cesar Suarez <cesar01032004@gmail.com>
2024-10-24 22:28:09 -0500

    Se ingresan valores desde teclado a la base de datos y añadimos gitignore

--- PythonApp/app.py
+++ PythonApp/app.py
@@ -1,20 +1,50 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
+import requests
 
-try:
-    coneccion = psycopg2.connect(
-        user="postgres",
-        password="cesarPSQL",
-        host="localhost",
-        port="5432",
-        database="DBApp"
+def obtener_ubicacion():
+    response = requests.get("http://ip-api.com/json/")
+    data = response.json()
+    return data['lat'], data['lon']
+
+def main():
+    latitud,longitud = obtener_ubicacion()
+    nombres = input("Ingrese su nombre: ")
+    apellidos = input("Ingrese su apellido: ")
+    accion = input("Accion(Ingreso o Salida): ")
+
+    load_dotenv()
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    database = os.getenv('DB_NAME')
+
+
+    try:
+        conection = psycopg2.connect(
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+            database=database
     )
 
-    cursor = coneccion.cursor()
-except (Exception, psycopg2.Error) as error:
-    print(f"Error al conectar con PostgreSQL: {error}")
+        cursor = conection.cursor()
+        query = f"insert into Asistencia (nombres, apellidos, latitud, longitud, accion) values (%s, %s, %s, %s, %s)"
+        values = (nombres, apellidos, latitud, longitud, accion)
+        cursor.execute(query, values)
+        conection.commit()
 
-finally:
-    if coneccion:
-        cursor.close()
-        coneccion.close()
-        print("Coneccion de PostgresSQL cerrada")
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error al conectar con PostgreSQL: {error}")
+
+    finally:
+        if conection:
+            cursor.close()
+            conection.close()
+            print("Coneccion de PostgresSQL cerrada")
+
+if __name__ == "__main__":
+    main()
